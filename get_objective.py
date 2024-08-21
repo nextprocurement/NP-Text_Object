03f704939ade93b6b4f076af39175c334ff14663
Nemesis1303 <lcalvo@pa.uc3m.es>
2024-08-21 13:50:55 +0200

    Adapted scripts for partitions preprocessing and checkpoints saving

--- get_objective.py
+++ get_objective.py
@@ -1,40 +1,53 @@
 import argparse
+import pathlib
 from src.objective_tender_extraction.objective_extractor import ObjetiveExtractor
 import pandas as pd
 import time
 
 if __name__ == '__main__':
-    
+
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, required=False, help='Path to the input file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_extracted/all_extracted_12aug_es.parquet")
-    parser.add_argument('--output', type=str, required=False, help='Path to the output file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives/all_extracted_12aug_es.parquet")
-    
-    args = parser.parse_args()
-    
-    oe = ObjetiveExtractor(do_train=False)
-    
-    df = pd.read_parquet(args.input)
-            
-    time_start = time.time()
-    try:
-        print("-- -- Extracting objectives")
-        df = oe.predict(df)
-    except Exception as e:
-        print(f'Error: {e}')
-    
-    time_end = time.time()
-    
-    print(
-        f'-- -- Time elapsed for objective extraction: {time_end - time_start}')
-    print(f'-- -- Saving extracted objectives to {args.output}')
-    df.to_parquet(args.output)
+    parser.add_argument('--input', type=str, required=False, help='Path to the input file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_extracted/all_extracted_12aug_es_parts")
+    # parser.add_argument('--input', type=str, required=False, help='Path to the input file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_extracted/all_extracted_12aug_es.parquet")
+    parser.add_argument('--output', type=str, required=False, help='Path to the output file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives/all_extracted_12aug_es")
+    # parser.add_argument('--output', type=str, required=False, help='Path to the output file', default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives/all_extracted_12aug_es.parquet")
+    parser.add_argument('--path_checkpoints', type=str, required=False, help='Path to the checkpoint files', default="/export/data_ml4ds/NextProcurement/PLACE/checkpoints")
 
-    print("-- -- STATS:")
-    len_no_found = len(df[df.objective == "/"])
-    avg_score = df[df.objective != "/"].in_text_score.mean()
-    print(f'-- -- -- Number of no found objectives: {len_no_found}')
-    print(f'-- -- -- Average score: {avg_score}')
-    
-    
-    
+    args = parser.parse_args()
+
+    oe = ObjetiveExtractor(do_train=False)
+
+    file_count = sum(1 for file in pathlib.Path(args.input).rglob('*') if file.is_file())
+    for el, path in enumerate(pathlib.Path(args.input).rglob('*')):
+        print("*" * 50)
+        print(f"-- -- Processing file {el+1} / {file_count}: {path}")
+        print("*" * 50)
+        path_checkpoint = pathlib.Path(args.path_checkpoints).joinpath(
+            f"{path.stem}_objectives.pkl")
+        path_save = pathlib.Path(args.output).joinpath(
+            f"{path.stem}_objectives.parquet")
+
+        df = pd.read_parquet(path)
+
+        time_start = time.time()
+        try:
+            print("-- -- Extracting objectives")
+            df = oe.predict(df, checkpoint_path=path_checkpoint.as_posix())
+        except Exception as e:
+            print(f'Error: {e}')
+
+        time_end = time.time()
+
+        print(
+            f'-- -- Time elapsed for objective extraction: {time_end - time_start}')
+
+        print(f'-- -- Saving extracted objectives to {path_save.as_posix()}')
+
+        df.to_parquet(path_save)
+
+        print("-- -- STATS:")
+        len_no_found = len(df[df.objective == "/"])
+        avg_score = df[df.objective != "/"].in_text_score.mean()
+        print(f'-- -- -- Number of no found objectives: {len_no_found}')
+        print(f'-- -- -- Average score: {avg_score}')
--- nlp_preprocess.py
+++ nlp_preprocess.py
@@ -1,44 +1,49 @@
 import argparse
 import pathlib
 import subprocess
+from tqdm import tqdm
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=False, help="Path to the input file", default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives/all_extracted_12aug_es.parquet")
-    parser.add_argument("--output", type=str, required=False, help="Path to the output file", default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives_preprocessed/all_extracted_12aug_es.parquet")
+    parser.add_argument("--input", type=str, required=False, help="Path to the input file", default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives/all_extracted_12aug_es")
+    parser.add_argument("--output", type=str, required=False, help="Path to the output file", default="/export/data_ml4ds/NextProcurement/PLACE/pliegos_objectives_preprocessed/all_extracted_12aug_es")
     
     args = parser.parse_args()
 
-    # Correct the replacement syntax for out_save
-    out_save = pathlib.Path(str(args.output).replace("all_extracted_12aug_es.parquet", "pliegos.parquet"))
-    
-    # Copy input file to temporary file
-    subprocess.run(["cp", args.input, str(out_save)])
+    for el in tqdm(pathlib.Path(args.input).rglob('*')):
+        
+        this_out_save = pathlib.Path(args.outout).joinpath(f"{el.stem}.parquet")
 
-    preprocessing_script = "/export/usuarios_ml4ds/lbartolome/NextProcurement/NP-Text_Object/src/preprocessing/pipe/nlpipe.py"
-    source_path = str(out_save)
-    source_type = "parquet"
-    source = "pliegos"
-    destination_path = args.output
-    spacy_model = "es_core_news_md"
-    lang = "es"
+        # Correct the replacement syntax for out_save
+        out_save = this_out_save.as_posix().replace(f"{el.stem}.parquet", "pliegos.parquet")
+        
+        # Copy input file to temporary file
+        subprocess.run(["cp", el.as_posix(), str(out_save)])
 
-    # Construct the command
-    cmd = [
-        "python", preprocessing_script,
-        "--source_path", source_path,
-        "--source_type", source_type,
-        "--source", source,
-        "--destination_path", destination_path,
-        "--lang", lang,
-        "--spacy_model", spacy_model
-    ]
+        preprocessing_script = "/export/usuarios_ml4ds/lbartolome/NextProcurement/NP-Text_Object/src/preprocessing/pipe/nlpipe.py"
+        source_path = str(out_save)
+        source_type = "parquet"
+        source = "pliegos"
+        destination_path = this_out_save
+        spacy_model = "es_core_news_md"
+        lang = "es"
 
-    try:
-        print(f'-- -- Running preprocessing command {" ".join(cmd)}')
-        subprocess.check_output(cmd)
-    except subprocess.CalledProcessError as e:
-        print('-- -- Preprocessing failed. Revise command')
-        print(e.output)
-    print("-- -- Preprocessing done")
+        # Construct the command
+        cmd = [
+            "python", preprocessing_script,
+            "--source_path", source_path,
+            "--source_type", source_type,
+            "--source", source,
+            "--destination_path", destination_path,
+            "--lang", lang,
+            "--spacy_model", spacy_model
+        ]
+
+        try:
+            print(f'-- -- Running preprocessing command {" ".join(cmd)}')
+            subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            print('-- -- Preprocessing failed. Revise command')
+            print(e.output)
+        print("-- -- Preprocessing done")
